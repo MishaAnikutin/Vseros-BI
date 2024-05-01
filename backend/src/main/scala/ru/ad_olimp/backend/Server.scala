0f241617a6e70b3aba3d822feec85af7c5281999
mishaanikutin <billyknife@mail.ru>
2024-05-01 19:29:39 +0300

    Описал микросервисную архитектуру в compose и nginx

--- backend/src/main/scala/ru/ad_olimp/backend/Server.scala
+++ backend/src/main/scala/ru/ad_olimp/backend/Server.scala
@@ -11,7 +11,7 @@ import ru.ad_olimp.backend.api.Router.{allRoutes => routes}
 override def run(args: List[String]): IO[ExitCode] = {
 
     val builder = BlazeServerBuilder[IO](executionContext)
-      .bindHttp(8080, "0.0.0.0")
+      .bindHttp(5000, "0.0.0.0")
       .withHttpApp(routes)
 
     val server = builder.resource.use(_ => IO.never).as(ExitCode.Success)
